66a7435ed523bdc24d5a69383c6b26168cc9e73d
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-31 11:37:49 +0300

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 5;
+int n = 4;
+double[,] array = new double [m, n];
+
+PrintArray(array);
