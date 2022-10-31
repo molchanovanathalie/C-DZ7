ef5c6c3c2bffd4c77d54172b0505986f8e3336c3
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-31 11:55:11 +0300

    add 002

---
+++ 002/Program.cs
@@ -0,0 +1,20 @@
+﻿// дать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void PrintArray(int[,] array)
+{
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            array[m, n] = m + n;
+            Console.Write($"{array[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 8;
+int n = 8;
+int[,] array = new int[m, n];
+
+
+PrintArray(array);
